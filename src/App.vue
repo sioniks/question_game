003da691bc902f844f6b3677bdbbfb622d7e2bee
Dzebliuk Oleksandr <dzebliuk.o@brights.cloud>
2019-07-05 12:52:00 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    
+    <StartScreen/>
+  </div>
+</template>
+
+<script>
+import StartScreen from './components/StartScreen.vue'
+
+export default {
+  name: 'app',
+  components: {
+    StartScreen
+  }
+}
+</script>
+
+<style lang="scss">
+
+#app {
+  background: red;
+  color: #000;
+}
+</style>
---
+++ src/components/StartScreen.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="start-screen">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StartScreen',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.start-screen {
+  background-image: "~/assets/image-background.png";
+}
+</style>
